e169eb872305fbb0b4e426a0a80a63e818b363db
Fiifi Dawson <edem.fiifi.dawson@gmail.com>
2023-04-30 22:20:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from tkinter import * # from tkinter import everything
 # Create a window
 root = Tk() # root is the name of the window
 
+
+# Override the settings of the window
 root.configure(bg="black") # sets the background color of the window
 
 root.geometry("1440x720") # sets the size of the window
@@ -13,4 +15,22 @@ root.title("Minesweeper") # sets the title of the window
 
 root.resizable(False, False) # makes the window not resizable
 
+top_frame = Frame(
+                root, 
+                bg="red",
+                width=1440, height=180
+)
+
+top_frame.place(x=0.0, y=0.0)
+
+left_frame = Frame(
+                root,
+                bg="blue",
+                width=360,
+                height=540
+)
+left_frame.place(x=0.0, y=180)
+
+
+# Run the window
 root.mainloop() # keeps the window open until we close it
